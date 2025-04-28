e0024e0a0b11bc2ec465ec1c990ee9819d499b44
ells-s <182009067+ells-s@users.noreply.github.com>
2025-04-28 15:04:21 +0100

    completed task 3 GET /api/articles/:article_id

--- app.js
+++ app.js
@@ -3,13 +3,31 @@ const app = express();
 const db = require("./db/connection");
 const { getApi } = require("./app/controllers/api.controller");
 const { getTopics } = require("./app/controllers/topics.controller");
+const { getArticleById } = require("./app/controllers/articles.controller");
 
 app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
 
-app.all('/*splat', (req, res) => {
+app.get("/api/articles/:article_id", getArticleById);
+
+app.all("/*splat", (req, res) => {
     res.status(404).send({ msg: "Not Found" });
+});
+
+app.use((err, req, res, next) => {
+    if (err.code === "22P02") {
+        res.status(400).send({ msg: "Bad Request" });
+    }
+    else {
+        next(err);
+    };
+});
+
+app.use((err, req, res, next) => {
+    if (err.status && err.msg) {
+        res.status(err.status).send({ msg: err.msg });
+    };
 });
 
 module.exports = app;
