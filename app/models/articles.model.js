0f0a66a9a686dca3e323305bc60a7f69c274a643
ells-s <182009067+ells-s@users.noreply.github.com>
2025-04-29 10:48:01 +0100

    completed task 4 GET /api/articles

--- app/models/articles.model.js
+++ app/models/articles.model.js
@@ -7,6 +7,13 @@ exports.selectArticleById = (article_id) => {
         });
 };
 
+exports.selectArticles = () => {
+    return db.query(`SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT (comments.comment_id) ::INT AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY created_at DESC`)
+        .then(({ rows }) => {
+            return rows;
+        });
+};
+
 exports.checkIfArticleExists = (article_id) => {
     return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
         .then(({ rows }) => {
