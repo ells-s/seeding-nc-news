16fb6162650404f74461e019e2f84d0051d211bc
ells-s <182009067+ells-s@users.noreply.github.com>
2025-04-30 10:50:58 +0100

    completed task 7 PATCH /api/articles/:article_id

--- app/models/articles.model.js
+++ app/models/articles.model.js
@@ -14,6 +14,13 @@ exports.selectArticles = () => {
         });
 };
 
+exports.updateArticleVotes = (inc_votes, article_id) => {
+    return db.query(`UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`, [inc_votes, article_id])
+        .then(({ rows }) => {
+            return rows[0];
+        });
+};
+
 exports.checkIfArticleExists = (article_id) => {
     return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
         .then(({ rows }) => {
