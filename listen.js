d37be70c99d07cc6762d981949d306943cf54f3e
ells-s <182009067+ells-s@users.noreply.github.com>
2025-05-01 13:38:49 +0100

    altered connection.js, listen.js and package.json

--- listen.js
+++ listen.js
@@ -1,9 +1,6 @@
 const app = require("./app");
 
-app.listen(9090, (err) => {
-    if (err) {
-        console.log("Can't connect to port");
-    } else {
-        console.log("Server is listening on port 9090");
-    }
-});
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+
